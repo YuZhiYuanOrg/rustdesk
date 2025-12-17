b631930fca5e4420ecca4e30efdefcf8e6ef3b1b
YuZhiYuanDev <203504060+YuZhiYuanDev@users.noreply.github.com>
2025-12-17 22:59:40 +0800

    updater

--- src/updater.rs
+++ src/updater.rs
@@ -120,14 +120,6 @@ fn start_auto_update_check_(rx_msg: Receiver<UpdateMsg>) {
 fn check_update(manually: bool) -> ResultType<()> {
     #[cfg(target_os = "windows")]
     let is_msi = crate::platform::is_msi_installed()?;
-    if !(manually || config::Config::get_bool_option(config::keys::OPTION_ALLOW_AUTO_UPDATE)) {
-        return Ok(());
-    }
-    if !do_check_software_update().is_ok() {
-        // ignore
-        return Ok(());
-    }
-
     let update_url = crate::common::SOFTWARE_UPDATE_URL.lock().unwrap().clone();
     if update_url.is_empty() {
         log::debug!("No update available.");
